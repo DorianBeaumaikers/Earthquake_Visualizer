e3d5fbc19a6472438733ad17739f0b79259b99cf
Dorian Beaumaikers <dorianbeaumaikers@gmail.com>
2022-06-16 17:02:51 +0200

    3D Earth

--- src/script.js
+++ src/script.js
@@ -17,14 +17,52 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 /**
- * Objects
+ * Textures
  */
-const object1 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
-);
+ const textureLoader = new THREE.TextureLoader();
+ const earthTexture = textureLoader.load('/textures/earth/earth_texture.jpg');
+ const earthHeightTexture = textureLoader.load('/textures/earth/earth_height.jpg');
+ const earthSpecularTexture = textureLoader.load('/textures/earth/earth_specular.jpg');
+ const cloudsTexture = textureLoader.load('/textures/earth/clouds.png');
 
-scene.add(object1);
+/**
+ * Earth
+ */
+ // Planet
+ const earth = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 32, 32),
+    new THREE.MeshPhongMaterial({
+        map: earthTexture,
+        displacementMap: earthHeightTexture,
+        displacementScale: 0.01,
+        specularMap: earthSpecularTexture,
+        specular: new THREE.Color('grey'),
+        shininess: 15
+    })
+);
+scene.add(earth);
+
+// Clouds
+const clouds = new THREE.Mesh(
+    new THREE.SphereGeometry(1.010, 32, 32),
+    new THREE.MeshPhongMaterial({
+        map: cloudsTexture,
+        transparent: true
+    })
+);
+scene.add(clouds);
+
+/**
+ * Lights
+ */
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+// Directional Light
+var directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.position.set(5,3,5);
+scene.add(directionalLight);
 
 /**
  * Sizes
@@ -53,19 +91,26 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 0.001, 1000);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+controls.minDistance = 1.2;
+controls.maxDistance = 3;
+controls.zoomSpeed = 0.2;
+controls.rotateSpeed = 0.3;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -78,6 +123,8 @@ const clock = new THREE.Clock();
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime();
+
+    clouds.rotation.y = elapsedTime * 0.005;
 
     // Update controls
     controls.update();
